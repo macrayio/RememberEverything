a98d8a057334e1f2395445fd1aff64a9784bc813
masesisaac <masesisaac@gmail.com>
2019-01-07 16:18:40 +0300

    structure

--- App.js
+++ App.js
@@ -2,18 +2,16 @@ import React from 'react'
 import { Platform, Text, View } from 'react-native'
 import { Constants } from 'expo'
 
-const App = () => {
-  return (
-    <View
-      style={{
-        // This is needed to avoid app crossing the status bar in some android phones
-        flex: 1,
-        marginTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
-      }}
-    >
-      <Text>Dem fuck start</Text>
-    </View>
-  )
-}
+const App = () => (
+  <View
+    style={{
+      // This is needed to avoid app crossing the status bar in some android phones
+      flex: 1,
+      marginTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
+    }}
+  >
+    <Text>Dem fuck start</Text>
+  </View>
+)
 
 export default App
